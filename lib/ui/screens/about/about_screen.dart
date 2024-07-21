ea32d5cba4ce8db577bc6bbec8f9e2bd51b10942
Webierta <webierta@gmail.com>
2024-07-21 10:52:03 +0200

    markdown locales

--- lib/ui/screens/about/about_screen.dart
+++ lib/ui/screens/about/about_screen.dart
@@ -1,11 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../states/settings_provider.dart';
 import '../../styles/styles_app.dart';
 import 'btc_wallet_address.dart';
-import 'markdown_about.dart';
+import 'markdown_locales.dart';
 import 'markdown_text.dart';
 
-class AboutScreen extends StatefulWidget {
+class AboutScreen extends ConsumerWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final idioma = ref.watch(settingsProvider).idioma;
+
+    String data1 = switch (idioma) {
+      'en' => MarkdownLocales.en1,
+      'es' => MarkdownLocales.es1,
+      _ => MarkdownLocales.en1,
+    };
+
+    String data2 = switch (idioma) {
+      'en' => MarkdownLocales.en2,
+      'es' => MarkdownLocales.es2,
+      _ => MarkdownLocales.en2,
+    };
+
+    return Container(
+      decoration: BoxDecoration(
+        gradient: StylesApp.gradient(Theme.of(context).colorScheme),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: const Text('About'),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  MarkdownText(data: data1),
+                  const BtcWalletAddress(),
+                  MarkdownText(data: data2),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/* class AboutScreen extends StatefulWidget {
   const AboutScreen({super.key});
 
   @override
@@ -42,4 +92,4 @@ class _AboutScreenState extends State<AboutScreen> {
       ),
     );
   }
-}
+} */
---
+++ lib/ui/screens/about/markdown_locales.dart
@@ -0,0 +1,156 @@
+import '../../../utils/consts.dart';
+
+const String urlPayPal =
+    'https://www.paypal.com/donate?hosted_button_id=986PSAHLH6N4L';
+const String urlGitHub = 'https://github.com/Webierta/phototook';
+const String urlGitHubIssues = 'https://github.com/Webierta/phototook/issues';
+
+class MarkdownLocales {
+  static const String en1 = """
+# PhotoTook
+
+#### META SEARCH ENGINE FOR PHOTOS ON THE BEST IMAGES PLATFORMS
+
+---
+
+**PhotoTook** searches for photos on top image platforms, including Unsplash, Pexels, Flickr, Pixabay and Openverse. In future revisions, these sources may be modified or expanded.
+
+The app accesses photos from the platforms through their API and has no official relationship with any of them. Each platform owns its registered trademark and logo and this application is not sponsored or supported by any.
+
+The app allows you to use some filters to search for photos, such as predominant color and orientation, although not all platforms support this filters.
+
+The application displays the images and associated information in different layouts, and allows some actions on the images: bookmark, zoom, share and download on your device.
+
+## Author and License
+
+> Version $version
+> Copyleft 2024. Hosted on [Github]($urlGitHub)
+> Jesús Cuerda (Webierta)
+> All Wrongs Reserved. Licencia GPLv3
+
+This app is freely shared under the terms of the *GNU General Public License v.3* in the hope that it will be useful, but WITHOUT ANY WARRANTY. This program is **free software**: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 (GPLv3). The GNU General Public License does not permit incorporating this program into proprietary programs.
+
+**PhotoTook** is *free and open source software*. Please consider contributing to keep the development of this App active. Thank you.
+
+Any suggestions, criticism, comments or bug reports are appreciated. Do you think you've found a problem? Identifying and fixing bugs makes this app better for everyone. Report a bug at [GitHub issues]($urlGitHubIssues).
+
+You can help the development of this and other apps with a small contribution via PayPal or Bitcoins. Thank you!
+
+Donate via Paypal (opens PayPal payment page):
+
+###### [![paypal](assets/images/paypal.png)]($urlPayPal)
+
+To make a donation in Bitcoins, scan this QR code with your wallet app
+
+###### ![Bitcoin_QR](assets/images/Bitcoin_QR.png)
+
+or copy my BTC wallet address:
+
+""";
+
+  static const String en2 = """
+
+---
+
+## Warranty, Security and Privacy
+
+**Free and ad-free application**. No user data is used. **Open source software** (source code on Github), free of spyware, malware, viruses or any process that threatens your device or violates your privacy. This application does not extract or store any information or require any external permission, and renounces advertising and any tracking mechanism.
+
+Each platform has its own terms and conditions and its own privacy and cookie policy, and images have different types of license and property rights for their use.
+
+It cannot be guaranteed that the content offered is free of errors, whether errors in the server from which the data originates, in the process of accessing the data or in its processing, so the information presented does not have any guarantee, either explicit or implicit. The user expressly agrees to be aware of this circumstance. The use of the information obtained by this App is carried out by the user at his own risk and expense, and he is solely responsible to third parties for any damages that may arise from it.
+
+---
+
+## Attributions
+
+### Images:
+
+* Home screen image of [Lisa Fotios](https://www.pexels.com/es-es/foto/persona-con-foto-de-un-solo-arbol-durante-el-dia-1252983/) on Pexels.
+
+* GIF of [David Montero](https://pixabay.com/es/users/editiox-6398285/?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=6613) on [Pixabay](https://pixabay.com/es//?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=6613).
+
+* GIF of [David Montero](https://pixabay.com/es/users/editiox-6398285/?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=6592) on [Pixabay](https://pixabay.com/es//?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=6592).
+
+### Fonts:
+
+* Nunito. Copyright 2014 [The Nunito Project Authors](https://github.com/googlefonts/nunito). Licensed under the SIL Open Font License, Version 1.1.
+
+* Major Mono Display. Copyright 2018 [The Major Mono Project Authors](https://github.com/googlefonts/majormono). Licensed under the SIL Open Font License, Version 1.1.
+
+""";
+
+  static const String es1 = """
+# PhotoTook
+
+#### METABUSCADOR DE FOTOS EN LAS MEJORES PLATAFORMAS DE IMÁGENES
+
+---
+
+**PhotoTook** busca fotos en las mejores plataformas de imágenes, como Unsplash, Pexels, Flickr, Pixabay y Openverse. En futuras revisiones, es posible que estas fuentes se modifiquen o amplíen.
+
+La aplicación accede a las fotos de las plataformas a través de su API y no tiene relación oficial con ninguna de ellas. Cada plataforma es propietaria de su marca registrada y logotipo y esta aplicación no está patrocinada ni respaldada por ninguna.
+
+La app permite utilizar algunos filtros para buscar fotos, como el color predominante y la orientación, aunque no todas las plataformas soportan estos filtros.
+
+La aplicación presenta las imágenes y la información asociada en diferentes diseños, y permite realizar algunas acciones sobre las imágenes: marcar, hacer zoom, compartir y descargar en tu dispositivo.
+
+## Autor y Licencia
+
+> Versión $version
+> Copyleft 2024. Código alojado en [Github]($urlGitHub)
+> Jesús Cuerda (Webierta)
+> All Wrongs Reserved. Licencia GPLv3
+
+Esta aplicación se comparte libremente bajo los términos de la *GNU General Public License v.3* con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA. Este programa es **software libre**: puede redistribuirlo y/o modificarlo bajo los términos de la Licencia Pública General GNU publicada por la Free Software Foundation, versión 3 (GPLv3). La Licencia Pública General GNU no permite incorporar este programa a programas propietarios.
+
+**PhotoTook** es *software libre y de código abierto*. Por favor, considera contribuir para mantener activo el desarrollo de esta App. Muchas gracias.
+
+Agradecemos cualquier sugerencia, crítica, comentario o informe de errores. ¿Crees que has encontrado un problema? Identificar y corregir errores hace que esta aplicación sea mejor para todos. Informa de un error en [GitHub issues]($urlGitHubIssues).
+
+Puedes ayudar al desarrollo de esta y otras aplicaciones con una pequeña contribución a través de PayPal o Bitcoins. Muchas gracias.
+
+Dona a través de Paypal (abre la página de pago de PayPal):
+
+###### [![paypal](assets/images/paypal.png)]($urlPayPal)
+
+Para hacer una donación en Bitcoins, escanea este código QR con tu aplicación monedero
+
+###### ![Bitcoin_QR](assets/images/Bitcoin_QR.png)
+
+o copia la dirección de mi monedero BTC:
+
+""";
+
+  static const String es2 = """
+
+---
+
+## Garantía, Seguridad y Privacidad
+
+**Aplicación gratuita y sin publicidad**. No se recaban ni utilizan datos del usuario. **Software de código abierto** (código fuente en Github), libre de spyware, malware, virus o cualquier proceso que amenace tu dispositivo o viole tu privacidad. Esta aplicación no extrae ni almacena ninguna información ni requiere ningún permiso externo, y renuncia a la publicidad y a cualquier mecanismo de rastreo.
+
+Cada plataforma tiene sus propios términos y condiciones y su propia política de privacidad y cookies, y las imágenes tienen diferentes tipos de licencia y derechos de propiedad para su uso.
+
+No se puede garantizar que el contenido ofrecido esté libre de errores, ya sean errores en el servidor del que proceden los datos, en el proceso de acceso a los datos o en su tratamiento, por lo que la información presentada no tiene ninguna garantía, ni explícita ni implícita. El usuario acepta expresamente ser consciente de esta circunstancia. La utilización de la información obtenida por esta App se realiza por el usuario por su propia cuenta y riesgo, siendo el único responsable frente a terceros de cualquier daño o perjuicio que pudieran derivarse de la misma.
+
+---
+
+## Atribuciones
+
+### Imágenes:
+
+* Imagen de pantalla de inicio de [Lisa Fotios](https://www.pexels.com/es-es/foto/persona-con-foto-de-un-solo-arbol-durante-el-dia-1252983/) on Pexels.
+
+* GIF de [David Montero](https://pixabay.com/es/users/editiox-6398285/?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=6613) en [Pixabay](https://pixabay.com/es//?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=6613).
+
+* GIF de [David Montero](https://pixabay.com/es/users/editiox-6398285/?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=6592) en [Pixabay](https://pixabay.com/es//?utm_source=link-attribution&utm_medium=referral&utm_campaign=animation&utm_content=6592).
+
+### Fuentes tipográficas:
+
+* Nunito. Copyright 2014 [The Nunito Project Authors](https://github.com/googlefonts/nunito). Licensed under the SIL Open Font License, Version 1.1.
+
+* Major Mono Display. Copyright 2018 [The Major Mono Project Authors](https://github.com/googlefonts/majormono). Licensed under the SIL Open Font License, Version 1.1.
+
+""";
+}
